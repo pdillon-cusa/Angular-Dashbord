8753f5584ec998ca62d47c960bf8199669d5d599
Peter Dillon <q12721@corp.cusa.canon.com>
2018-06-15 17:07:16 -0400

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sku Maintenance' },
+  { id: 12, name: 'Promotion Maintenance' },
+  { id: 13, name: 'Manage CP Types' },
+  { id: 14, name: 'Activation Support' }
+];
